5fa43824aca46a4e9973bd7f7335226832924e9c
Akseli <s2100140@edu.bc.fi>
2021-03-25 13:29:20 +0200

    first components done. Trying to commit from boxes directly

--- boxes/src/App.js
+++ boxes/src/App.js
@@ -1,12 +1,34 @@
-import logo from "./logo.svg";
 import "./App.css";
+
+const Box = (props) => {
+  return (
+    <div className="box">
+      <h2>Name: {props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Title: {props.title}</p>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <div>Hello from Arabianranta!</div>
+    <div>
+      <header>
+        <h1>Logo</h1>
       </header>
+      <main>
+        <Box name="Sakarius Topelius" age="123" title="kirjailija" />
+        <Box name="Akseli Miettinen" age="65" title="Suurmestari" />
+        <Box name="Fantastic Fred" age="1050" title="Grand Wizard" />
+        <div className="box" id="myId">
+          <h2>Name: Manually Written</h2>
+          <p>Age: 10</p>
+          <p>Title: Dr.</p>
+        </div>
+      </main>
+      <footer>
+        <p>Copyright</p>
+      </footer>
     </div>
   );
 }
