8cb600dc59e9c14c5f94916bdd0e6cdcae4fe81b
Akseli <s2100140@edu.bc.fi>
2021-03-29 14:43:32 +0300

    getting counter working also with colors

--- boxes/src/Footer.js
+++ boxes/src/Footer.js
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <nav>
+    <footer>
       <p>Copyright</p>
-    </nav>
+    </footer>
   );
 };
 
--- boxes/src/Main.js
+++ boxes/src/Main.js
@@ -1,24 +1,115 @@
-import React from "react";
-import "./App.css";
+// import "./App.css";
 
-const Box = (props) => {
-  return (
-    <div className="box">
-      <h2>Name: {props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Title: {props.title}</p>
-    </div>
-  );
-};
+class Main extends Component {
+  state = {
+    persons: [
+      {
+        name: "Tupu",
+        age: 50,
+        department: "sales",
+      },
+      {
+        name: "Hupu",
+        age: 55,
+        department: "developing",
+      },
+      {
+        name: "Lupu",
+        age: 45,
+        department: "marketing",
+      },
+      { name: "Jim", age: 50, department: "sales" },
+      {
+        name: "Oscar",
+        age: 55,
+        department: "finance",
+      },
+      {
+        name: "Pam",
+        age: 45,
+        department: "front desk",
+      },
+    ],
+  };
 
-const Main = () => {
-  return (
-    <div>
-      <Box name="Sakarius Topelius" age="123" title="kirjailija" />
-      <Box name="Akseli Miettinen" age="65" title="Suurmestari" />
-      <Box name="Fantastic Fred" age="1050" title="Grand Wizard" /> );
-    </div>
-  );
+  clicetyClick = () => {
+    console.log("here we go again!");
+    this.setState({
+      persons: [
+        {
+          name: "Tupu",
+          age: 50,
+          department: "aviation",
+        },
+        {
+          name: "Hupu",
+          age: 55,
+          department: "coding",
+        },
+        {
+          name: "Lupu",
+          age: 45,
+          department: "sanitary",
+        },
+        { name: "Jim", age: 50, department: "assistant to regional manager" },
+        {
+          name: "Oscar",
+          age: 55,
+          department: "accounting",
+        },
+        {
+          name: "Pam",
+          age: 45,
+          department: "reception",
+        },
+      ],
+    });
+  };
+
+  render() {
+    return (
+      <main>
+        <button onClick={this.clicetyClick}>click here...if you DARE!</button>
+        <div>
+          <Box
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            department={this.state.persons[0].department}
+          />
+          <Box
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            department={this.state.persons[1].department}
+          />
+          <Box
+            name={this.state.persons[2].name}
+            age={this.state.persons[2].age}
+            department={this.state.persons[2].department}
+          />
+          <Box
+            name={this.state.persons[3].name}
+            age={this.state.persons[3].age}
+            department={this.state.persons[3].department}
+          />
+          <Box
+            name={this.state.persons[4].name}
+            age={this.state.persons[4].age}
+            department={this.state.persons[4].department}
+          />
+          <Box
+            name={this.state.persons[5].name}
+            age={this.state.persons[5].age}
+            department={this.state.persons[5].department}
+          />
+        </div>
+      </main>
+    );
+  }
+}
+let clicks = 0;
+const countClick = () => {
+  clicks++;
+  console.log("click " + clicks);
 };
 
 export default Main;
