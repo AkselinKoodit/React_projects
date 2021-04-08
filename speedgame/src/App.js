34766e2b8bade1be6625661915fabb6de31eff89
Akseli <s2100140@edu.bc.fi>
2021-04-08 15:56:41 +0300

    added some styling and tried to get score showing, but it seems to be bit tricky with react because XSS attack risk...

--- speedgame/src/App.js
+++ speedgame/src/App.js
@@ -3,6 +3,8 @@ import Circle from "./components/Circle";
 
 import "./App.css";
 
+let scores = 0;
+
 class App extends Component {
   state = {
     circles: [
@@ -13,19 +15,33 @@ class App extends Component {
     ],
   };
   clickHandler = (circle) => {
-    alert("Hi there! I'm " + circle);
+    scores++;
+    console.log(`You clicked ${circle} and you score is ${scores}`);
+
+    document.getElementsByClassName("score").dangerouslySetInnerHTML = {
+      _html: `You clicked ${circle} and you score is ${scores}`,
+    };
   };
   render() {
     const circlesList = this.state.circles.map((circle) => {
-      return <Circle id={circle.id} key={circle.id} color={circle.color} />;
+      return (
+        <Circle
+          id={circle.id}
+          key={circle.id}
+          color={circle.color}
+          clickMe={this.clickHandler.bind(this, circle.color)}
+        />
+      );
     });
     return (
       <div>
         <h1>Speedtest</h1>
-        <p>Score: </p>
+        <p className="score">Score: </p>
         <div className="circles">{circlesList}</div>
-        <button>Start</button>
-        <button>Stop</button>
+        <div className="controls">
+          <button>Start</button>
+          <button>Stop</button>
+        </div>
       </div>
     );
   }
