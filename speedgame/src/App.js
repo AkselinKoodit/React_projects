b05656f11a578d9cb51288e46a7cd92f04b56b77
Akseli <s2100140@edu.bc.fi>
2021-04-08 14:57:59 +0300

    speedgame basics done with Margit

--- speedgame/src/App.js
+++ speedgame/src/App.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
+import Circle from "./components/Circle";
+
+import "./App.css";
 
 class App extends Component {
+  state = {
+    circles: [
+      { id: 1, color: "green" },
+      { id: 2, color: "brown" },
+      { id: 3, color: "orange" },
+      { id: 4, color: "blue" },
+    ],
+  };
+  clickHandler = (circle) => {
+    alert("Hi there! I'm " + circle);
+  };
   render() {
-    return <div></div>;
+    const circlesList = this.state.circles.map((circle) => {
+      return <Circle id={circle.id} key={circle.id} color={circle.color} />;
+    });
+    return (
+      <div>
+        <h1>Speedtest</h1>
+        <p>Score: </p>
+        <div className="circles">{circlesList}</div>
+        <button>Start</button>
+        <button>Stop</button>
+      </div>
+    );
   }
 }
 
